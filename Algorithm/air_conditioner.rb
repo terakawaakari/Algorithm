e6b2c9ffdb8b252e84c190a00f0c9d8e4addf279
terakawaakari <terakawaakari0701@gmail.com>
2021-05-21 06:14:48 +0000

    air_conditionerに風量による時間の変化を追加

--- Algorithm/air_conditioner.rb
+++ Algorithm/air_conditioner.rb
@@ -2,6 +2,7 @@ input_lines = readlines
 
 room = input_lines[0].to_i
 air_conditioner = input_lines[1].to_i
+power = input_lines[2].to_i
 
 temperature_diff = (room - air_conditioner).abs
 
@@ -9,10 +10,22 @@ require_time = 0
 
 if temperature_diff < 5
   require_time = 15
-elsif temperature_diff > 4 && temperature_diff < 10 then
+elsif temperature_diff > 4 && temperature_diff < 10
   require_time = 30
-elsif temperature_diff > 10 then
+elsif temperature_diff > 10
   require_time = 60
+end
+
+case power
+when 1
+  require_time
+when 2
+  require_time -= 5
+when 3
+  require_time -= 10
+else
+  puts "風量の値は１〜３にしてください"
+  exit
 end
 
 p require_time
